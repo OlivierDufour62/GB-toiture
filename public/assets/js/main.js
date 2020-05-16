1321132a24e8939a5b00a209b35826797fb21a05
Olivier Dufour <57214448+crashmania@users.noreply.github.com>
2020-05-17 00:49:52 +0200

    header footer

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
-    $(".mobilemenu").click(function () {
-        $(`.mm`).toggle('slow');
+    $(`.mobilemenu`).click(function () {
+        $(`.mm`).toggle(`slow`);
+    });
+
+    $(`.hov`).hover(function () {
+        $(this).addClass(`border264d7e`);
+    }, function () {
+        $(this).removeClass(`border264d7e`);
     });
 });
